94dfca70c04e24b942a65d719ffe3d90b25b4c39
Donatas <90237321+Barbadosas89@users.noreply.github.com>
2021-12-21 21:35:24 +0200

    Update index.php

--- 2021-12-21/index.php
+++ 2021-12-21/index.php
@@ -4,7 +4,7 @@ $url = 'https://randomuser.me/api/?format=csv';
 
 $users = file_get_contents($url);
 
-file_put_contents('users.xls', $users, FILE_APPEND);
+file_put_contents('users.csv', $users, FILE_APPEND);
 
 
 
